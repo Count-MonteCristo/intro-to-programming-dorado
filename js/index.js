f92bf30726d067aa76b81f4efece47c86c788662
Count-MonteCristo <luis0425navarro@gmail.com>
2022-10-28 11:28:54 -0500

    created css file and stylized the site. also modified HTML and JS

--- js/index.js
+++ js/index.js
@@ -9,7 +9,24 @@ copyright.innerHTML = `Luis Navarro ${thisYear}`;
 footer.appendChild(copyright);
 
 //creates list of skills
-let skills = ["HTML/CSS/JavaScript", "UX/UI", "WordPress", "Copywriting"];
+let skills = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "C/C++",
+  "Java",
+  "Python",
+  "SQL",
+  "WordPress",
+  "Bash",
+  "Linux",
+  "UX/UI",
+  "Sitemaps",
+  "Wireframes",
+  "Mockups",
+  "Prototypes",
+  "Copywriting",
+];
 let skillSection = document.getElementById("skills");
 let skillsList = skillSection.querySelector("ul");
 
@@ -17,6 +34,13 @@ for (let i = 0; i < skills.length; i++) {
   let skill = document.createElement("li");
   skill.innerHTML = skills[i];
   skillsList.appendChild(skill);
+}
+
+document.getElementById("messages").style.display = "none";
+
+//function to determine if list is empty, used to hide messages section as needed
+function isEmpty(id) {
+  return document.getElementById(id).innerHTML.trim() == "";
 }
 
 //handles message form submits
@@ -32,23 +56,32 @@ messageForm.addEventListener("submit", (e) => {
   console.log(formEmail.value);
   console.log(formMessage.value);
 
-  //Displays messages in a list
+  //displays messages in a list
   let messageSelection = document.getElementById("messages");
   let messageList = messageSelection.querySelector("ul");
 
   let newMessage = document.createElement("li");
   newMessage.innerHTML = `<a href="mailto:${formEmail.value}"> ${formName.value} </a> <span> wrote: ${formMessage.value} </span>`;
 
+  //removes messages from list
   let removeButton = document.createElement("button");
   removeButton.textContent = "Remove";
   removeButton.type = "button";
   removeButton.addEventListener("click", (e) => {
     let entry = newMessage.querySelector("button").parentNode;
     entry.remove();
+    if (isEmpty("list") == true) {
+      document.getElementById("messages").style.display = "none";
+    }
   });
 
   newMessage.appendChild(removeButton);
   messageList.appendChild(newMessage);
+
+  //asseses the status of the list
+  if (isEmpty("list") == false) {
+    document.getElementById("messages").style.display = "block";
+  }
 
   messageForm.reset();
 });
